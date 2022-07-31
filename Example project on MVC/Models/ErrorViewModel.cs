188534316f5488a76048a7cd57ef074bd71262c7
enfan <enfan@DESKTOP-LGDEA04>
2022-07-31 08:54:44 +0300

    Добавьте файлы проекта.

---
+++ Example project on MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Example_project_on_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
